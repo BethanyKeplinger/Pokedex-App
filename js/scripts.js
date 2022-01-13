250c94ae6f75f85748eca84f434509e3052c06ba
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-01-13 16:17:05 -0500

    Update scripts.js
    Create a new list of Pokemon-including name, height and type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 
-alert('Hello world');
-
-let favoriteFood = 'Coffee ice cream'
-document.write(favoriteFood)
+let pokemonList = [
+  {name: 'Jigglypuff', height: .5, type: ['fairy', 'normal']},
+  {name: 'Oddish' , height: .5, type: ['grass', 'poison']}.
+  {name: 'Slowpoke', height: 1.2, type['psychic', 'water']}
+];
