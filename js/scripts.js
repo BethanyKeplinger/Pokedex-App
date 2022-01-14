8152f2c6a6a9ef165e308580c30477383a87150c
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-01-14 08:34:15 -0500

    Update scripts.js
    Fixed minor punctuation errors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 let pokemonList = [
   {name: 'Jigglypuff', height: .5, type: ['fairy', 'normal']},
-  {name: 'Oddish' , height: .5, type: ['grass', 'poison']}.
-  {name: 'Slowpoke', height: 1.2, type['psychic', 'water']}
+  {name: 'Oddish' , height: .5, type: ['grass', 'poison']},
+  {name: 'Slowpoke', height: 1.2, type: ['psychic', 'water']}
 ];
