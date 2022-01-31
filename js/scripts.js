7d971c2b4c07a52d43d6b552ec28cfc8dc280969
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-01-31 16:30:54 -0500

    Update scripts.js
    Fixed some styling . Added addListItem function, showDetails function and getAll function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,56 @@
-
 const pokemonRepository = (() => {
-let pokemonList = [
-  { name: 'Jigglypuff',
+  let repository = [
+  {
+    name: 'Jigglypuff',
     height: .5,
-    type: ['fairy', 'normal']
+    type: ['fairy', 'normal'],
   },
 
-  { name: 'Beedrill' ,
+  {
+    name: 'Beedrill',
     height: 1,
-    type: ['bug', 'poison']
+    type: ['bug', 'poison'],
   },
 
-  { name: 'Slowpoke',
+  {
+    name: 'Slowpoke',
     height: 1.2,
-    type: ['psychic', 'water']
-  }
+    type: ['psychic', 'water'],
+  },
 ];
 
-return {
-  add: function (pokemon) {
-    pokemonList.push(pokemon);
-  },
-  getAll: function () {
-    return pokemonList;
+  function getAll() {
+    return repository;
   }
-};
+
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    listpokemon.appendchild(button);
+    pokemonList.appendChild(listpokemon);
+
+    //adding eventhandler to button to show pokemon
+    button.addEventListener('click', function (event) {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+  };
 })();
 
-// for(let i = 0; i < pokemonList.length; i++) {
-//   if (pokemonList[i].height > 1) {
-//   document.write (
-//     pokemonList[i].name +
-//     ' (height:' +
-//     pokemonList[i].height +
-//     'm)-' +
-//     ' Wow, that\'s a big pokemon!' +
-//     '<br>'
-//   )
-// } else
-//   document.write(
-//   pokemonList[i].name +
-//   ' (height: ' +
-//   pokemonList[i].height +
-//   'm)'+
-//   '<br>'
-//   );
-// }
+console.log(pokemonRepository.getAll());
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-  console.log(pokemon.name + ' is ' + pokemon.height + ' meters tall ');
+  pokemonRepository.addListItem(pokemon);
 });
