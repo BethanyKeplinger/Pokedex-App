c5d0da5a1e61d092cf17daf2168157c0dd5f1e2d
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-02-07 12:31:28 -0500

    Update scripts.js
    Added comments for clarity. Added Modal function for pokemon list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 let pokemonRepository = (function () {
+    let modalContainer = document.querySelector('#modal-container');
     let repository = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+    //function adds pokemon to list
     function add(pokemon) {
       if (
         typeof pokemon === 'object' &&
@@ -14,6 +16,7 @@ let pokemonRepository = (function () {
       }
     }
 
+    //function creates button for pokemon list
     function addListItem(pokemon) {
       let pokemonList = document.querySelector('.pokemon-list');
       let listpokemon = document.createElement('li');
@@ -21,13 +24,15 @@ let pokemonRepository = (function () {
       button.innerText = pokemon.name;
       button.classList.add('pokemon-button');
       listpokemon.appendChild(button);
-      pokemonList.appendChild(listpokemon);
+      pokemonList.appendChild(listpokemonmn);
 
+      //Add event listener to button
       button.addEventListener('click', function (event) {
         showDetails(pokemon);
       });
     }
 
+    //function loads list from api
     function loadList() {
       return fetch(apiUrl).then(function (response) {
         return response.json();
@@ -49,6 +54,7 @@ let pokemonRepository = (function () {
       return repository;
     }
 
+    //function to fetch details for each pokemon from api
     function loadDetails(item) {
       let url = item.detailsUrl;
       return fetch(url).then(function (response) {
@@ -64,11 +70,74 @@ let pokemonRepository = (function () {
       });
     }
 
+    //function shows pokemon name, height and image
     function showDetails(item) {
       pokemonRepository.loadDetails(item).then(function () {
-        console.log(item);
+        showModal(pokemon.name, 'Height: ' + pokemon.height, pokemon.imageUrl);
       });
     }
+
+    //Start Modal function
+    function showModal(title, text, imgSrc) {
+      let modalContainer = document.querySelector('#modal-container');
+
+      //Clear all existing modal content
+      modalContainer.innerHTML = '';
+
+      let modal = document.createElement('div');
+      modal.classList.add('modal');
+
+      //Add the new modal content
+      let closeButtonElement = document.createElement('button');
+      closeButtonElement.classList.add('modal-close');
+      closeButtonElement.innerText = 'Close';
+      closeButtonElement.addEventListener('click', hideModal);
+
+      let titleElement = document.createElement('h1');
+      titleElement.innerText = title;
+
+      let contentElement = document.createElement('p');
+      contentElement.innerText = text;
+
+      let modalImg = document.createElement('img');
+      modalImg.classList.add('modal-img');
+      modalImg.src = imgSrc;
+
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(contentElement);
+      modal.appendChild(modalImg);
+      modalContainer.appendChild(modal);
+
+      //makes modal visible
+      modalContainer.classList.add('is-visible');
+    }
+
+    function hideModal() {
+      modalContainer.classList.remove('is-visible');
+    }
+
+    //Using ESC key to exit out of modal
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' &&
+      modalContainer.classList.contains('is-visible')) {
+        hideModal();
+      }
+    });
+
+    //CLosing modal by clicking outside the container
+    modalContainer.addEventListener('click', (e) => {
+      // Since this is also triggered when clicking INSIDE the modal
+      // We only want to close if the user clicks directly on the overlay
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+
+    document.querySelector('#show-modal').addEventListener('click', () => {
+      showModal('Modal title', 'This is the modal content!');
+    });
 
     return {
       add: add,
